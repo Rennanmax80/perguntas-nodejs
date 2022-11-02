c1b4f1522970e6cdd519f7e8fbbdaf35d13df7ca
Rennan <rennanmax80@gmail.com>
2022-11-02 15:24:03 -0300

    model respostas e tela respostas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv/config")
 const bodyParser = require("body-parser")
 const conection = require('./database/database')
 const Pergunta = require('./database/Pergunta.model')
+const Resposta = require('./database/Resposta.model')
 
 //database
 conection.authenticate().then(() => {
@@ -41,7 +42,17 @@ app.get("/pergunta/:id", (req, res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined){ //pergunta encontrada
-            res.render("pergunta")
+
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                })
+            })
+
         }else{ // N encontrada
             res.redirect("/")
         }
@@ -62,5 +73,20 @@ app.post("/salvarpergunta", (req, res) => {
         res.redirect("/")
     })
 })
+
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId,
+    }).then(() => {
+        res.redirect('/pergunta/'+perguntaId) 
+    }).catch()
+
+})
+
+
 
 app.listen(process.env.PORT, () => console.log("Servidor iniciado")) 
